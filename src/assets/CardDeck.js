4f0e7bf1a73a5ef15e384cecb4cb4bae254e5901
Okko Ailio <oiaili@utu.fi>
2025-03-27 09:26:38 +0200

    Changed to full deck, fixed dealer's 2nd card animation clipping

--- src/assets/CardDeck.js
+++ src/assets/CardDeck.js
@@ -5,10 +5,10 @@ const suits = {
   spades: '/spade.webp',
 };
 
-// const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10','J','Q', 'K', 'A']
+const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10','J','Q', 'K', 'A']
 // TURN THIS ^^^ ON
 // small values for debugging
-const ranks = ['2', '3', '4', '5', '6', '7']
+// const ranks = ['2', '3', '4', '5', '6', '7']
 // const ranks = ['K', 'A']
 
 const combinations = Object.entries(suits).flatMap(([suit, image]) =>
